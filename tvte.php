0821255907c25e5eb8ed986f8e6602b26abd6796
tivitim <59048021+tivitim@users.noreply.github.com>
2019-12-20 00:07:44 +0300

    Add files via upload

---
+++ tvte.php
@@ -0,0 +1,378 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<items>
+<channel>
+    <title>TR:BEIN SPORTS HABER UHD</title>
+    <description></description>
+    <stream_url><![CDATA[http://86a.yasamiptv.com:8080/live/app2Ta23/At7Qb92Cv91Z/18057.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN SPORTS 1 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN SPORTS 1 UHD</title>
+    <description></description>
+    <stream_url><![CDATA[http://maxu.yasamiptv.com:8080/live/app2Ta23/At7Qb92Cv91Z/37268.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN SPORTS 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/bein2222/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN SPORTS 3 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/bein3333/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN SPORTS 4 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/bein4444/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN SPORTS MAX 1 UHD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/beinmax1111/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN SPORTS MAX 2 UHD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/beinmax2222/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:TIVIBUSPOR 1 UHD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/tivibu11/chunks.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:TIVIBUSPOR 2 UHD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/tivibu22/chunks.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:TIVIBUSPOR 3 UHD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/tivibu33/chunks.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:S SPORT HD</title>
+    <description></description>
+    <stream_url><![CDATA[https://www.underoverbet10.com:443/live/12_.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:SMART SPOR  HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://86a.yasamiptv.com:8080/live/app2Ta23/At7Qb92Cv91Z/89334.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:EUROSPORT 1 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/eurosport11/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:EUROSPORT 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/eurosport22/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN BOX OFFICE 1 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinbox1/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN BOX OFFICE 1 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinbox1/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN BOX OFFICE 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinbox2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN BOX OFFICE 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinbox2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN BOX OFFICE 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinbox3/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEIN BOX OFFICE 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinbox3/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES ACTİON HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesaction/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES ACTİON HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesaction/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES ACTİON 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesaction2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES ACTİON 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesaction2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES PREMİER HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviespremier/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES PREMİER HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviespremier/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES STARS HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesstars/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES STARS HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesstars/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES FAMLY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesfamily/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES FAMLY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesfamily/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES TÜRK HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesturk/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN MOVİES TÜRK HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinmoviesturk/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVİES SMART ACTİON HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msaction/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVİES SMART ACTİON HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msaction/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVİES SMART FAMİLY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msfamily/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVİES SMART FAMİLY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msfamily/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVİES SMART TURK HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msturk/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVİES SMART TURK HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msturk/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PLATİN HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msplatin/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PLATİN HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msplatin/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PLATİN 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msplatin2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PLATİN 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/msplatin2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PREMİUM HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/mspremium/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PREMİUM HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/mspremium/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PREMİUM 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/mspremium2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:MOVlES SMART PREMİUM 2 HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/mspremium2/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN SERİES SCİ Fİ HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinseriesscfi/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN SERİES SCİ Fİ HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinseriesscfi/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN SERİES VİCE HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinseriesvice/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN SERİES VİCE HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinseriesvice/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN SERİES COMEDY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinseriescomedy/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BEİN SERİES COMEDY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/beinseriescomedy/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:ANİMASYON HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/animasyon/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:ANİMASYON HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/animasyon/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:İMDB HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/imdb/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:İMDB HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportssinema/imdb/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:NATIONAL GEOGRAPHIC HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/nationalgeographic/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:NATIONAL GEOGRAPHIC HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/nationalgeographic/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:NAT GEO PEOPLE HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/natgeopeople/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:NAT GEO PEOPLE HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/natgeopeople/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:NATIONAL WILD HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/natgeowild/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:NATIONAL WILD HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/natgeowild/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:DISCOVERY CHANNEL HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/discoverychannel/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:DISCOVERY CHANNEL HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/discoverychannel/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:DISCOVERY SCIENCE HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/discoverysceince/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:DISCOVERY SCIENCE HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/discoverysceince/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:HISTORY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/history/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:HISTORY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/history/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BBC EART HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/bbcearth/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:BBC EART HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/bbcearth/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR: ANIMAL PLANET HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/animalplanet/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:DAVINCI LEARNING HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportscizgifilm/davinci/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:VIASAT HISTORY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/viasat/playlist.m3u8]]></stream_url>
+</channel>
+<channel>
+    <title>TR:VIASAT HISTORY HD</title>
+    <description></description>
+    <stream_url><![CDATA[http://yayin22.bunusanasoracagim.best:80/selcuksportsbelgesel/viasat/playlist.m3u8]]></stream_url>
+</channel>
+</items>
