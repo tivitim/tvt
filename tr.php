65a730e7ed6a84dc656e1bfc795ec8d22b626a80
tivitim <59048021+tivitim@users.noreply.github.com>
2019-12-20 11:26:06 +0300

    Add files via upload

---
+++ tr.php
@@ -0,0 +1,18 @@
+﻿<?php
+$link = "http://yayin11.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8";
+$veri = file_get_contents($link);
+preg_match('#m3u8(.*?)"#',$veri,$veritemp);
+//echo $veritemp[1];
+//print_r($veritemp[1]);
+function go ($url, $time = 0){
+        if ($time) header("Refresh: {$time}; url={$url}");
+        else header("Location: {$url}");
+    }
+
+    go("http://yayin11.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8$veritemp[1]");
+?>
+
+
+
+
+
