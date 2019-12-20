727c6f13b70a54909fa8c8acc39325c0aeb14a99
tivitim <59048021+tivitim@users.noreply.github.com>
2019-12-20 14:16:19 +0300

    Add files via upload

---
+++ tvt.php
@@ -0,0 +1,6 @@
+﻿#EXTM3U
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 UHD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 UHD
+https://egoldstudio.com/tvt/beinspor1.php
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 UHD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 UHD
+https://egoldstudio.com/tvt/beinspor2.php
