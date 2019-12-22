22247e3d6a273ad33da1139927af3c17bce788af
tivitim <59048021+tivitim@users.noreply.github.com>
2019-12-22 16:46:33 +0300

    Add files via upload

---
+++ tvt.php
@@ -0,0 +1,3 @@
+﻿#EXTM3U
+##EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS HABER UHD" tvg-logo="http://kde.ipne.be/logoslar/bein-sports-haber-qa-tr.png" group-title="TR SPOR",TR:BEIN SPORTS HABER UHD
+http://86a.yasamiptv.com:8080/live/app2Ta23/At7Qb92Cv91Z/18057.m3u8
