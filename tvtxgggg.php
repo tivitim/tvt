d8f43a9b958c3d3a2fbb2a29eac45665bad9460d
tivitim <59048021+tivitim@users.noreply.github.com>
2019-12-23 20:08:18 +0300

    Add files via upload

---
+++ tvtxgggg.php
@@ -0,0 +1,8 @@
+﻿#EXTM3U
+
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 HD
+http://iptvh.it/beinactionm3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 2 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" group-title="TR SPOR",TR:BEIN SPORTS 2 HD
+http://iptvh.it/beinactionm3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 3 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports3hd.png" group-title="TR SPOR",TR:BEIN SPORTS 3 HD
+http://iptvh.it/beinactionm3u8
