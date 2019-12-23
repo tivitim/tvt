fbde9e5cc1f1297a78feac6713397ba922643d49
tivitim <59048021+tivitim@users.noreply.github.com>
2019-12-23 14:28:43 +0300

    Add files via upload

---
+++ tvtx.php
@@ -0,0 +1,3 @@
+﻿#EXTM3U
+##EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS HABER UHD" tvg-logo="http://kde.ipne.be/logoslar/bein-sports-haber-qa-tr.png" group-title="TR SPOR",TR:BEIN SPORTS HABER UHD
+http://yayin11.bunusanasoracagim.best:80/selcuksportsfutbol/eurosport22/playlist.m3u8
